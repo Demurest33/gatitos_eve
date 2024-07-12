3882d7f5ba294a16050d9540458fa4f24f1f2a52
javierjararenteria@gmail.com <202160023@ucc.mx>
2024-07-12 13:16:45 -0600

    Banner Horizontal

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), icon()],
   site: "https://demurest.github.io",
-  base: "/gatitos_eve/",
+  base: "/gatitos_eve/"
 });
